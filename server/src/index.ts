4834fe7b129062a18440ef084aec235c32191be3
serhii.shramko <shramko.web@yahoo.com>
2020-08-20 09:40:43 +0300

    feat: implement simple server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,30 @@
 import express from 'express';
 
+import { listings } from "./listings";
+import bodyParser from "body-parser";
+
 const PORT = 9000;
 const app = express();
+app.use(bodyParser.json());
 
-const one = 1;
-const two = 2;
+// TODO add routes folder
+app.get("/listings", (req, res) => {
+  return res.send(listings);
+});
 
-app.get("/", (req, res) => res.send(`1 + 2 = ${one + two}`))
+app.post('/delete-listing', (req, res, next) => {
+  const id: string = req.body.id;
+  console.log(id, listings)
+
+  for (let i = 0; i < listings.length; i++) {
+    if(listings[i].id === id) {
+      const removedItem = listings.splice(i, 1);
+      return res.send(removedItem)
+    }
+  }
+
+  return res.send('Failed to remove item');
+});
 
 app.listen(PORT);
-
 console.log(`[app]: http://localhost:${PORT}`);
