2ea229f25612839aed3ddfba9701a8d9dcb239fd
serhii.shramko <shramko.web@yahoo.com>
2020-08-23 15:21:56 +0300

    feat: implement base GraphQL api

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,31 +1,17 @@
-import express from 'express';
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
 
-import { listings } from "./listings";
-import bodyParser from "body-parser";
+import {
+  resolvers,
+  typeDefs
+} from "./graphql";
 
 const PORT = 9000;
 const app = express();
-app.use(bodyParser.json());
 
-// TODO add routes folder
-app.get("/listings", (req, res) => {
-  return res.send(listings);
+const server = new ApolloServer({typeDefs, resolvers});
+server.applyMiddleware({app, path: "/api"});
+
+app.listen(PORT, () => {
+  console.log(`[app]: http://localhost:${PORT}`);
 });
-
-// TODO rewrite to DELETE with :params ID ?
-app.post('/delete-listing', (req, res, next) => {
-  const id: string = req.body.id;
-  console.log(id, listings)
-
-  for (let i = 0; i < listings.length; i++) {
-    if (listings[i].id === id) {
-      const removedItem = listings.splice(i, 1);
-      return res.send(removedItem)
-    }
-  }
-
-  return res.send(`Failed to remove item: id:${id}`);
-});
-
-app.listen(PORT);
-console.log(`[app]: http://localhost:${PORT}`);
