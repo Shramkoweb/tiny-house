24cd2475561ecccc48d2f01fe926bf291ce6b748
serhii.shramko <shramko.web@yahoo.com>
2020-08-25 22:32:38 +0300

    build: add mongoDB setup add db types.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,27 @@
-import express from "express";
+import express, { Application } from "express";
 import { ApolloServer } from "apollo-server-express";
 
 import {
   resolvers,
   typeDefs
 } from "./graphql";
+import { connectDatabase } from "./database";
 
-const PORT = 9000;
 const app = express();
+const PORT = 9000;
 
-const server = new ApolloServer({typeDefs, resolvers});
-server.applyMiddleware({app, path: "/api"});
+const mount = async (app: Application) => {
+  const db = await connectDatabase();
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: () => ({db})
+  });
+  server.applyMiddleware({app, path: "/api"});
 
-app.listen(PORT, () => {
-  console.log(`[app]: http://localhost:${PORT}`);
-});
+  app.listen(PORT, () => {
+    console.log(`[app]: http://localhost:${PORT}`);
+  });
+};
+
+mount(app);
