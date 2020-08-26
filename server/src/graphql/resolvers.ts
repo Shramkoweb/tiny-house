ef3a30d400891e9bb752168ad20607749d68a8e1
serhii.shramko <shramko.web@yahoo.com>
2020-08-26 10:18:55 +0300

    refactor: update GraphQL resolvers access mongoDB data remove listings.ts mock module

--- server/src/graphql/resolvers.ts
+++ server/src/graphql/resolvers.ts
@@ -1,24 +1,40 @@
 import { IResolvers } from "apollo-server-express";
-import { listings } from "../listings";
+import { ObjectId } from "mongodb";
+import {
+  Database,
+  Listing
+} from "../lib/types";
 
 export const resolvers: IResolvers = {
-  Query: {
-    listings: () => {
-      return listings;
-    }
-  },
-  Mutation: {
-    deleteListing: (_root: undefined, {id}: { id: string }) => {
-      for (let i = 0; i < listings.length; i++) {
-        if (listings[i].id === id) {
-          // splice return array of removed item('s) so we need destruct first item
-          const [removedItem] = listings.splice(i, 1);
-
-          return removedItem;
-        }
+    Query: {
+      listings: async (
+        _root: undefined,
+        _args: Record<string, unknown>,
+        {db}: { db: Database }
+      ): Promise<Listing[]> => {
+        // find({}) return all documents from db
+        return await db.listings.find({}).toArray();
       }
+    },
+    Mutation: {
+      deleteListing: async (
+        _root: undefined,
+        {id}: { id: string },
+        {db}: { db: Database }
+      ): Promise<Listing> => {
+        const deleteResult = await db.listings.findOneAndDelete({
+          _id: new ObjectId(id)
+        });
 
-      throw new Error(`Failed to remove item: id:${id}`);
+        if (!deleteResult.value) {
+          throw new Error(`[db] : failed to delete element}`);
+        }
+
+        return deleteResult.value;
+      }
+    },
+    Listing: {
+      id: (listing: Listing) => listing._id.toString()
     }
   }
-};
+;
