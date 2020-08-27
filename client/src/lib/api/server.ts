3c206c10e38daf07e2c9ecbccb78e17be739f5f0
serhii.shramko <shramko.web@yahoo.com>
2020-08-27 19:48:47 +0300

    feat: add api folder & useQuery hook

---
+++ client/src/lib/api/server.ts
@@ -0,0 +1,22 @@
+interface Body<TVariables> {
+  query: string;
+  variables?: TVariables
+}
+
+export const server = {
+  fetch: async <TResponseData = any, TRequestVariables = any>(
+    body: Body<TRequestVariables>
+  ) => {
+    console.log(JSON.stringify(body));
+    const res = await fetch("/api", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body)
+    });
+
+    return res.json() as Promise<{ data: TResponseData }>;
+  }
+};
+
