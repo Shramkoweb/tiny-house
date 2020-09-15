8322e9790e72030e0c45809c91a1eb7338d75501
serhii.shramko <shramko.web@yahoo.com>
2020-09-15 21:05:37 +0300

    refactor: useQuery & useMutation state's to useReducer hook

--- client/src/lib/api/useMutation.ts
+++ client/src/lib/api/useMutation.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useReducer } from "react";
 
 import { server } from "./server";
 
@@ -8,25 +8,52 @@ interface State<TData> {
   error: boolean;
 }
 
+type Action<TData> =
+  | { type: "FETCH" }
+  | { type: "FETCH_SUCCESS"; payload: TData }
+  | { type: "FETCH_ERROR" };
+
+const reducer = <TData>() => (
+  state: State<TData>,
+  action: Action<TData>
+) => {
+  switch (action.type) {
+    case "FETCH":
+      return {...state, loading: true};
+    case "FETCH_SUCCESS":
+      return {
+        ...state,
+        data: action.payload,
+        loading: false,
+        error: false
+      };
+    case "FETCH_ERROR":
+      return {...state, loading: false, error: true};
+    default:
+      throw new Error();
+  }
+};
+
 export const useMutation = <TData = any, TVariables = any>(query: string) => {
-  const [state, setState] = useState<State<TData>>({
+  const getReducer = reducer<TData>();
+
+  const [state, dispatch] = useReducer(getReducer, {
     data: null,
     loading: false,
-    error: false,
+    error: false
   });
 
   const fetch = async (variables?: TVariables) => {
     try {
-      setState({data: null, loading: true, error: false});
-
+      dispatch({type: "FETCH"});
       const {data, errors} = await server.fetch<TData, TVariables>({query, variables});
 
       if (errors && errors.length) {
         throw new Error(errors[0].message);
       }
-      setState({data, loading: false, error: false});
+      dispatch({type: "FETCH_SUCCESS", payload: data});
     } catch (error) {
-      setState({data: null, loading: false, error: true});
+      dispatch({type: "FETCH_ERROR"});
       throw console.error(error.message);
     }
   };
--- client/src/lib/api/useQuery.ts
+++ client/src/lib/api/useQuery.ts
@@ -1,7 +1,7 @@
 import {
   useCallback,
   useEffect,
-  useState
+  useReducer,
 } from "react";
 
 import { server } from "./server";
@@ -12,8 +12,39 @@ interface State<TData> {
   error: boolean;
 }
 
+// TODO useQuery & useMutation refactor interface to equal { fetch: function, state: { data, loading, error} }
+// TODO DRY Action & reducer
+type Action<TData> =
+  | { type: "FETCH" }
+  | { type: "FETCH_SUCCESS"; payload: TData }
+  | { type: "FETCH_ERROR" };
+
+const reducer = <TData>() => (state: State<TData>, action: Action<TData>): State<TData> => {
+  switch (action.type) {
+    case "FETCH":
+      return {
+        ...state,
+        loading: true,
+      };
+    case "FETCH_SUCCESS":
+      return {
+        data: action.payload,
+        loading: false,
+        error: false,
+      };
+    case "FETCH_ERROR":
+      return {
+        ...state,
+        loading: false,
+        error: true,
+      };
+    default:
+      throw new Error("Unknown action type");
+  }
+};
 export const useQuery = <TData = any>(query: string) => {
-  const [state, setState] = useState<State<TData>>({
+  const getReducer = reducer<TData>();
+  const [state, dispatch] = useReducer(getReducer, {
     data: null,
     loading: false,
     error: false,
@@ -21,24 +52,14 @@ export const useQuery = <TData = any>(query: string) => {
 
   const fetchApi = useCallback(async () => {
     try {
-      setState({
-        data: null,
-        loading: true,
-        error: false
-      });
-
+      dispatch({type: "FETCH"});
       const {data, errors} = await server.fetch<TData>({query});
       if (errors && errors.length) {
         throw new Error(errors[0].message);
       }
-      setState({data, loading: false, error: false});
+      dispatch({type: "FETCH_SUCCESS", payload: data});
     } catch (error) {
-      // set error flag
-      setState({
-        data: null,
-        loading: false,
-        error: true
-      });
+      dispatch({type: "FETCH_ERROR"});
       throw console.error(error.message);
     }
   }, [query]);
