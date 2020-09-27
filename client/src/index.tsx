cfcdffef9aa384f622cd0d7bac15c2da2dae248d
serhii.shramko <shramko.web@yahoo.com>
2020-09-27 20:46:42 +0300

    feat: add static types generating & schema for GraphQL remove custom hooks & types

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { render } from "react-dom";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 
 import { Listings } from "./sections";
 
+const client = new ApolloClient({
+  uri: "http://localhost:9000/api"
+});
+
 render(
-  <React.StrictMode>
-    <Listings title='Tiny House'/>
-  </React.StrictMode>,
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <Listings title='Tiny House'/>
+    </React.StrictMode>
+  </ApolloProvider>,
   document.getElementById("root")
 );
